b1c4ed1748d705d06f992aae1851ffd0870293f2
davisantosr <davi-rocha2@hotmail.com>
2020-12-04 20:10:46 -0300

    api with JsonResponse|pure django|Nested relationships

--- products/urls.py
+++ products/urls.py
@@ -5,10 +5,16 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 # from .views import ProductDetailView, ProductListView
-from .views import product_list, product_detail
+from .views import (
+  manufacturer_detail, manufacturer_list, product_list, product_detail, )
 
 urlpatterns = [
   path("products/", product_list, name='product-list'),
   path("products/<int:pk>/", product_detail, name='product-detail'),
+
+  path("manufacturers/", manufacturer_list, name='manafacturers-list'),
+  path("manufacturers/<int:pk>/", manufacturer_detail, name='manafacturers-list'),
+
+
 
 ]
